b58583d22cf92d6d099e32cb23037dd5a6315ffe
zhangzhulei <13339687922@163.com>
2020-02-01 16:28:27 +0800

    推荐页面翻页动画实现

--- src/components/home/FlapCard.vue
+++ src/components/home/FlapCard.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flap-card-wrapper" v-show="flapCardVisible">
-		<div class="flap-card-bg">
+		<div class="flap-card-bg" :class="{'animation': runFlapCardAnimation}">
 			<div class="flap-card" v-for="(item,index) in flapCardList" :key="index" :style="{zIndex: item.zIndex}">
 				<div class="flap-card-circle">
 					<div class="flap-card-semi-circle">
@@ -26,7 +26,10 @@
 			return {
 				flapCardList,
 				front: 0,
-				back: 1
+				back: 1,
+				intervalTime: 25,
+				task: null,
+				runFlapCardAnimation: false
 			}
 		},
 		methods: {
@@ -44,8 +47,6 @@
 				const item = this.flapCardList[index]
 				let dom;
 				if (type === 'front') {
-					// eslint-disable-next-line no-console
-					console.log(111111111)
 					dom = this.$refs.right[index]
 				} else {
 					dom = this.$refs.left[index]
@@ -53,21 +54,93 @@
 				dom.style.transform = `rotateY(${item.rotateDegree}deg)`;
 				dom.style.backgroundColor = `rgb(${item.r},${item._g},${item.b})`;
 			},
-			startFlapCardAnimation() {
-				setInterval(() => {
-					const frontFlapCard = this.flapCardList[this.front]
-					const backFlapCard = this.flapCardList[this.back]
-					frontFlapCard.rotateDegree += 10
-					frontFlapCard._g -= 5
-					backFlapCard.rotateDegree -= 10
+			flapCardRotate() {
+				const frontFlapCard = this.flapCardList[this.front];
+				const backFlapCard = this.flapCardList[this.back];
+				frontFlapCard.rotateDegree += 10
+				frontFlapCard._g -= 5
+				backFlapCard.rotateDegree -= 10
+				if (backFlapCard.rotateDegree < 90) {
 					backFlapCard._g += 5
-					this.rotate(0,'front')
-					this.rotate(1,'back')
-				}, 500)
+				}
+				if (frontFlapCard.rotateDegree === 90 && backFlapCard.rotateDegree === 90) {
+					backFlapCard.zIndex += 2
+				}
+				this.rotate(this.front,'front');
+				this.rotate(this.back,'back');
+				if (frontFlapCard.rotateDegree === 180 && backFlapCard.rotateDegree === 0) {
+					this.next()
+				}
+			},
+			next() {
+				const frontFlapCard = this.flapCardList[this.front];
+				const backFlapCard = this.flapCardList[this.back];
+				frontFlapCard.rotateDegree = 0;
+				backFlapCard.rotateDegree = 0;
+				frontFlapCard._g = frontFlapCard.g;
+				backFlapCard._g = backFlapCard.g;
+				this.rotate(this.front, 'front');
+				this.rotate(this.back, 'back');
+				this.front++;
+				this.back++;
+				const len = this.flapCardList.length
+				if (this.front >= len) {
+					this.front = 0
+				}
+				if (this.back >= len) {
+					this.back = 0
+				}
+				this.flapCardList.forEach((item, index) => {
+					item.zIndex = 100 - ((index - this.front + len) % len)
+				})
+				this.prepare()
+			},
+			prepare() {
+				const backFlapCard = this.flapCardList[this.back];
+				backFlapCard.rotateDegree = 180;
+				backFlapCard._g = backFlapCard.g - 5 * 9;
+				this.rotate(this.back, 'back')
+			},
+			reset() {
+				this.front = 0;
+				this.back = 1;
+				this.flapCardList.forEach((item, index) => {
+					item.zIndex = 100 - index;
+					item._g = item.g;
+					item.rotateDegree = 0;
+					this.rotate(index, 'front');
+					this.rotate(index, 'back');
+				});
+			},
+			startFlapCardAnimation() {
+				this.prepare();
+				this.task = setInterval(() => {
+					this.flapCardRotate()
+				}, this.intervalTime)
+			},
+			stopAnimation() {
+				if (this.task) {
+					clearInterval(this.task)
+				}
+				this.reset()
+			},
+			runAnimation() {
+				this.runFlapCardAnimation = true
+				setTimeout(() => {
+					this.startFlapCardAnimation()
+				}, 300)
 			}
 		},
-		mounted() {
-			this.startFlapCardAnimation()
+		watch: {
+			flapCardVisible(val) {
+				if (val) {
+					this.runAnimation()
+
+				} else {
+					this.runFlapCardAnimation = false
+					this.stopAnimation()
+				}
+			}
 		}
 	}
 </script>
@@ -85,6 +158,27 @@
 			height: px2rem(64);
 			border-radius: px2rem(5);
 			background-color: #ffffff;
+			&.animation{
+				animation: flap-card-move .3s ease-in;
+			}
+			@keyframes flap-card-move {
+				0% {
+					transform: scale(0);
+					opacity: 0;
+				}
+				50% {
+					transform: scale(1.2);
+					opacity: 1;
+				}
+				75% {
+					transform: scale(0.9);
+					opacity: 1;
+				}
+				100% {
+					transform: scale(1);
+					opacity: 1;
+				}
+			}
 			.flap-card {
 				position: absolute;
 				top: 0;
